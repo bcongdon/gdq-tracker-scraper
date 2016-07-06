d4c3e3a0371c5244a3432c0ebdea6a72958d4df6
Ben Congdon <bcongdo2@illinois.edu>
2016-07-05 18:32:18 -0700

    Bid detail scraper

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getBidIndex(cb) {
           description: data[2],
           amount: data[3],
           goal: data[4],
-          bid_ref: $(this).find('a').attr('href').split('/').pop()
+          bid_ref: $(this).find('a').attr('href').split('/').slice(1).join('/')
         }
       }).get();
     })
@@ -22,4 +22,54 @@ function getBidIndex(cb) {
     });
 }
 
-getBidIndex(console.log)
+function getBidDetail(ref, cb) {
+  scraperjs.StaticScraper.create('https://gamesdonequick.com/tracker/bid/' + ref)
+    .scrape(function($) {
+      var data =  $('tr').map(function() {
+        var data = $(this).children().map(function() {
+          return $(this).text().replace(/\r?\n|\r/g, "");
+        }).get();
+        var link = $(this).find('a').attr('href')
+        if(link) {
+          data.push(link.split('/').slice(1).join('/'))
+        }
+        return [data]
+      }).get()
+      var type = BID_TYPE.UNKNOWN;
+      if(data[2][0] == 'Name') type = BID_TYPE.CHOICE;
+      if(data[2][0] == 'NameAscDsc') type = BID_TYPE.GOAL;
+      data = data.slice(3);
+      data = data.map(function(d){
+        var payload = {
+          name: d[0],
+          amount: d[2]
+        }
+        if(d[1]) payload.date = Date.parse(d[1]);
+        if(d.length > 3) payload.link = d.pop()
+        return payload
+      });
+      var info = $('h2 > small').text().split("\n").slice(1,-1);
+      var payload = {
+        type: type,
+        data: data,
+        bid: info[1],
+        total: info[3]
+      }
+      if(info.length > 5) payload.goal = info[5];
+      return payload;
+    })
+    .then(function(data) {
+      cb(data)
+    });
+}
+
+var BID_TYPE = {
+  UNKNOWN: 'UNKNOWN',
+  GOAL: 'GOAL',
+  CHOICE: 'CHOICE',
+}
+
+getBidDetail(4539, console.log);
+getBidDetail(4540, console.log);
+getBidDetail(4826, console.log);
+
